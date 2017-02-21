d7225beb6b79ea1b7f08c479fbe1951d9a5c9f44
martynenkoav <martynenkoav.18@mail.ru>
2017-02-21 21:23:49 +0300

    Initial commit

---
+++ src/First.java
@@ -0,0 +1,13 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Scanner;
+
+/**
+ * Created by Анастасия on 22.01.2017.
+ */
+public class First {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+    }
+}
